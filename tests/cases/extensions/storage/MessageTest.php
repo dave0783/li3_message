9de711c53066614b40a8fdb398cbde949386e034
Rob Spencer <rob@robspencer.net>
2013-04-13 11:31:42 -0400

    Fixing broken unit tests

--- tests/cases/extensions/storage/MessageTest.php
+++ tests/cases/extensions/storage/MessageTest.php
@@ -17,29 +17,29 @@ class MessageTest extends \lithium\test\Unit {
 
     public function testWrite() {
         Message::write('Foo');
-        $expected = array('message' => 'Foo', 'atts' => array());
+        $expected = array('message' => 'Foo', 'options' => array());
         $result = Session::read('Message.default', array('name' => 'default'));
         $this->assertEqual($expected, $result);
 
         Message::write('Foo 2', array('type' => 'notice'));
-        $expected = array('message' => 'Foo 2', 'atts' => array('type' => 'notice'));
+        $expected = array('message' => 'Foo 2', 'options' => array('type' => 'notice'));
         $result = Session::read('Message.default', array('name' => 'default'));
         $this->assertEqual($expected, $result);
 
         Message::write('Foo 3', array(), 'TestKey');
-        $expected = array('message' => 'Foo 3', 'atts' => array());
+        $expected = array('message' => 'Foo 3', 'options' => array());
         $result = Session::read('Message.TestKey', array('name' => 'default'));
         $this->assertEqual($expected, $result);
     }
 
     public function testWriteCustomType() {
-        Message::notice('key', 'value');
-        $expected = array('message' => 'value', 'atts' => array());
+        Message::notice('key', 'value', array('class' => 'alert'));
+        $expected = array('message' => 'value', 'options' => array('class' => 'alert'));
         $result = Session::read('Message.notice.key');
         $this->assertEqual($expected, $result);
 
         Message::error('key', 'value', array('class' => 'error'));
-        $expected = array('message' => 'value', 'atts' => array('class' => 'error'));
+        $expected = array('message' => 'value', 'options' => array('class' => 'error'));
         $result = Session::read('Message.error.key');
         $this->assertEqual($expected, $result);
     }
